e2fdb89aa3f06d9d18d67b155fed49783c0610ae
Supakorn Netsuwan <earth_killerdark@hotmail.com>
2022-04-18 18:57:27 +0700

    change game 2 css tag

--- game2/game2.js
+++ game2/game2.js
@@ -6,35 +6,46 @@ let position = {
     y: -100
 }
 
-let availableFuel = 100
+let availableFuel = 120
 
 const newPosition = (x, y, deg, force) => {
     const changeToRad = Number(deg) * (Math.PI / 180)
     const newX = x + (Number(force) * Math.cos(changeToRad))
     const newY = y + (Number(force) * Math.sin(changeToRad))
 
+        console.log({x, y})
     return ({
         x: newX,
         y: newY
     })
 }
 
+const checkVictory = ({x, y}) => {
+    //Test for victory position
+    // const victoryPlanet = document.getElementsByClassName("victory-planet")[0];
+
+    if((x >= 1060 && x <= 1260) && ( y <= -130 && y >= -420)){
+        alert("Victory!")
+    }
+}
+
 fireButton.addEventListener("click", () => {
     const getDeg = document.getElementsByName("input-degree")[0] // Get a degree
     const getForce = document.getElementsByName("input-force")[0] // get a force
     const spaceShip = document.getElementById("space-ship");
-
     //Decrease fuel on every fire
     const currentFuel = document.getElementById("current-fuel");
 
+    
     if (availableFuel - Math.abs(Number(getForce.value / 10)) <= 0) {
         let direction = getForce.value < 0 ? -1 : 1;
         const lastPower = availableFuel * 10 * direction;
-
+        
         position = newPosition(position.x, position.y, getDeg.value, lastPower)
         spaceShip.style.left = position.x + "px"
         spaceShip.style.top = -position.y + "px"
-
+        checkVictory(position);
+        
         availableFuel = 0
         currentFuel.style.width = availableFuel + "%"
     } else {
@@ -44,6 +55,7 @@ fireButton.addEventListener("click", () => {
         position = newPosition(position.x, position.y, getDeg.value, getForce.value)
         spaceShip.style.left = position.x + "px"
         spaceShip.style.top = -position.y + "px"
+        checkVictory(position);
     }
 
 
